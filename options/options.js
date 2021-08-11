60018e4c00eaf018f34887515498681c54df5a7c
Crowdbotics <team@crowdbotics.com>
2021-08-11 15:47:13 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "one_life_city_churc_29721Identifier",
+  url: "https://one_life_city_churc_29721Identifier.botics.co",
+  api: "https://one_life_city_churc_29721Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
